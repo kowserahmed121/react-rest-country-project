8df031937dd0960d942556a4b958dad4d7cd6064
kowserahmed121 <kowser.dev@gmail.com>
2024-10-27 00:16:05 +0600

    complete world  country with react

--- src/Components/Component/Component.jsx
+++ src/Components/Component/Component.jsx
@@ -1,21 +1,55 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
-import './component.css'
+import "./component.css";
 const Component = () => {
-    const [country, countryState] = useState([])
-    useEffect(()=>{
-        fetch("https://restcountries.com/v3.1/all")
-        .then(res => res.json())
-        .then(data => countryState(data))
-    },[])
-    return (
-        <div className="component-style">
-            {
-                country.map(country => <Country key={country.cca3} country={country}></Country>)
-            }
-        </div>
-    );
+  const [country, countryState] = useState([]);
+  const [visitCountry, setVisitCountry] = useState([]);
+
+  const [visitFlag, setVisitFlag] = useState([]);
+
+  const handleState = (country) => {
+    const newVisitCountry = [...visitCountry, country];
+    setVisitCountry(newVisitCountry);
+  };
+
+  const showHandleFlag = (flag) => {
+    const newFlag = [...visitFlag, flag];
+    setVisitFlag(newFlag);
+  };
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => countryState(data));
+  }, []);
+  return (
+    <div>
+      <div>
+        <h3>Visited Country : {visitCountry.length}</h3>
+        <ul>
+          {visitCountry.map((con) => (
+            <li className="list-style" key={country.cca3}>
+              {con.name.common}
+            </li>
+          ))}
+        </ul>
+        {visitFlag.map((flag) => (
+          <img className="img-width" key={flag.cca3l} src={flag.flags.png} />
+        ))}
+      </div>
+      <div className="component-style">
+        {country.map((country) => (
+          <Country
+            key={country.cca3}
+            country={country}
+            handleState={handleState}
+            showHandleFlag={showHandleFlag}
+          ></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Component;
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import "./Country.css";
-const Country = ({ country }) => {
+const Country = ({ country, handleState, showHandleFlag }) => {
   const [visit, setVisit] = useState(false);
   const handleVisit = () => {
     setVisit(!visit);
   };
   const { name, flags, capital, continents, population, region } = country;
   return (
-    <div className="country-style">
-      <h3>Country Name : {name.common}</h3>
+    <div className={`country-style ${visit && "visit-country"}`}>
+      <h3 style={{ color: visit ? "tomato" : "black" }}>
+        Country Name : {name.common}
+      </h3>
       <div className="flex">
         <p>Continent: {continents}</p>
         <p>Capital: {capital}</p>
@@ -20,8 +22,22 @@ const Country = ({ country }) => {
       <div className="img-div">
         <img className="width" src={flags.png} alt="" />
       </div>
-      <button onClick={handleVisit} className="visitBtn">
-        {visit ? "visited" : "want visit"}
+      <div className="gap-div">
+        <button onClick={handleVisit} className="visitBtn">
+          {visit ? "visited" : "want-visit"}
+        </button>
+        {visit && "  complete visit this country"}
+        <button onClick={() => handleState(country)} className="visitBtn">
+          mark
+        </button>
+      </div>
+      <button
+      className="visitBtn"
+        onClick={() => {
+          showHandleFlag(country);
+        }}
+      >
+        show flag
       </button>
     </div>
   );
